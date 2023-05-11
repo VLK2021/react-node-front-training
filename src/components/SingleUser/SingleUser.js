ad239f7e514b5b95b51b288ea7dab6eb37039cfe
vlk2022 <volodymyrkostiuk2021@gmail.com>
2023-05-11 17:58:49 +0300

    update user from sql

--- src/components/SingleUser/SingleUser.js
+++ src/components/SingleUser/SingleUser.js
@@ -1,25 +1,53 @@
-import React from 'react';
-
+import React, {useState} from 'react';
 import {useLocation} from "react-router-dom";
+
 import {userService} from "../../services";
+import './SingleUserStyle.css';
+import {useForm} from "react-hook-form";
 
 
 const SingleUser = () => {
+    const [visible, setVisible] = useState(true);
+    const {register, handleSubmit, reset} = useForm();
+
     const location = useLocation();
     const {user} = location.state;
-    const {id, name} = user;
+    const {id, name, email, city} = user;
 
     const deleteUser = async () => {
         // await fetch(`http://localhost:5000/users/${id}`, {method:'DELETE'})
         await userService.deleteUser(id);
     }
 
+    const submit = async (data) => {
+        const {email, password, city} = data;
+        await userService.updateUser(id, {email, password, city});
+        reset();
+    }
+
 
     return (
         <div className={'singleUser'}>
-            <div>{id}) {name}</div>
+            <div className={'singleUser-inf'}>{id}) {name} {email} {city}</div>
+
+            {visible &&
+            <div className={'popApp'}>
+                <h1>update user</h1>
+                <form onSubmit={handleSubmit(submit)}>
+                    <label>Email<input type="text" {...register('email')}/></label>
+                    <label>Password<input type="text" {...register('password')}/></label>
+                    <label>City<input type="text" {...register('city')}/></label>
+
+                    <div className={'btn'}>
+                        <button>update user</button>
+                    </div>
+                </form>
+
+            </div>
+            }
 
             <button onClick={deleteUser}>delete from sql</button>
+            <button onClick={() => setVisible(!visible)}>update user in sql</button>
         </div>
     );
 };
--- src/services/userService.js
+++ src/services/userService.js
@@ -1,9 +1,10 @@
 import {axiosService} from "./axiosService";
-import {urls} from "../components/constants";
+import {urls} from "../constants";
 
 
 export const userService = {
     getAll: () => axiosService.get(urls.users),
     createUs: (data) => axiosService.post(urls.createUser, data),
     deleteUser: (id) => axiosService.delete(urls.users + `/${id}`),
+    updateUser: (id, {email, password, city}) => axiosService.put(urls.users + `/${id}`, {email, password, city})
 }
